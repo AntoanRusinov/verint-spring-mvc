a8e502741d14155bde94d6d053ea66484bcc6b58
Antoan Rusinov <antoan.rusinov@gmail.com>
2016-11-14 12:31:15 +0200

    commit all

---
+++ src/main/java/application/VerintSpringMvcApplication.java
@@ -0,0 +1,12 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerintSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerintSpringMvcApplication.class, args);
+	}
+}
